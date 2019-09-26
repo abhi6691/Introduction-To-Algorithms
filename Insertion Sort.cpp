42f62f72d8655bd63a37b9202026be49787c9f9b
Abhishek Singh <Abhi@smritis-MacBook-Air.local>
2019-09-26 17:03:07 +0530

    Insertion Sort Code

---
+++ Insertion Sort.cpp
@@ -0,0 +1,51 @@
+/**********************************************************************************************************************************************************
+	Author: Abhishek Singh
+	Problem link/description: Insertion Sort
+**********************************************************************************************************************************************************/
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+typedef vector <int> vi;
+
+void printVec(vi v){for(int i = 0; i < v.size(); i++) cout<<v[i]<<" "; cout<<endl;}
+
+void InsertionSort(vi &v)
+{
+	int n = v.size();
+
+	for (int i = 1; i < n; i++)
+	{
+		int key = v[i], j = i-1;
+		while (j >= 0 && v[j] > key)
+		{
+			v[j+1] = v[j];
+			j--;
+		}
+
+		v[j+1] = key;
+	}
+}
+
+int main()
+{
+	freopen("/Users/Abhi/Desktop/Job/inputf.in", "r", stdin);
+	freopen("/Users/Abhi/Desktop/Job/outputf.in", "w", stdout);
+
+	int n;
+	vi v;
+
+	cin>>n;
+	for (int i = 0; i < n; i++)
+	{
+		int t;
+		cin>>t;
+		v.push_back(t);
+	}
+
+	InsertionSort(v);
+	printVec(v);
+
+	return 0;
+}
